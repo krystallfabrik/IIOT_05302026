c65efb6ff635c3f8ce100ad6d76a4e7148e8f15a
krystallfabrik <112928276+krystallfabrik@users.noreply.github.com>
2026-05-31 23:38:21 -0500

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "kfabrik-645975910827-300526-bucket-hv4u8jx11xdjjm"
+    key     = "development/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+
+  }
+}
