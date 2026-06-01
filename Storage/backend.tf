5845d11930718b029de9436f5df99807a0b034c9
krystallfabrik <krystallfabrik@gmaill.com>
2026-06-01 03:29:38 -0500

    chng hard cod ed bucket to var

--- Storage/backend.tf
+++ Storage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.5"
   backend "s3" {
-    bucket  = "kfabrik-645975910827-300526-bucket-hv4u8jx11xdjjm"
+    bucket  = var.s3_bucket
     key     = "development/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
